2f89b3e9eeeebc176f78e762dc2e5b6727088dd6
vishavk1992 <vishavdeep551@gmail.com>
2024-05-06 13:37:01 -0400

    update and delete api

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const ProductList = () => {
     const [products, setProducts] = useState([])
@@ -12,8 +13,20 @@ const ProductList = () => {
         result = await result.json();
         setProducts(result);
     }
-    console.log("products", products)
+    // console.log("products", products)
 
+    const deleteProduct = async (id)=>{
+        let result = await fetch(`http://localhost:5000/product/${id}`,{
+            method : "Delete",
+        });
+        result = await result.json()
+        if(result){
+            alert("record is delete")
+        }
+     
+    }
+
+   
     return (
         <div className='product-list'>
             <h3>Product List</h3>
@@ -21,15 +34,23 @@ const ProductList = () => {
                 <li>S.No</li>
                 <li>Name</li>
                 <li>Price</li>
+                <li>Company</li>
                 <li>Category</li>
+                <li>Operation</li>
             </ul>
             {
                 products.map((item, index) => 
-                    <ul>
+                    <ul key={item._id}>
                         <li>{index+1}</li>
                         <li>{item.name}</li>
                         <li>{item.price}</li>
+                        <li>{item.company}</li>
                         <li>{item.category}</li>
+
+                        <li><button onClick={()=>deleteProduct(item._id)}>Delete</button>
+                        <Link to={"/update/"+ item._id}>Update</Link>   
+                        </li>
+                        {/* //we can also write in template literal with `` and $ */}
                     </ul>
                 )
             }
---
+++ src/components/UpdateProduct.js
@@ -0,0 +1,53 @@
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
+
+const UpdateProduct = () => {
+    const [name, setName] = useState("");
+    const [price, setPrice] = useState("");
+    const [category, setCategory] = useState("");
+    const [company, setCompany] = useState("");
+    const params = useParams();
+
+    useEffect(()=>{       
+        getProductDetails();
+    },[]);
+
+    const getProductDetails = async() =>{
+        console.log(params);
+        let result = await fetch(`http://localhost:5000/product/${params.id}`);
+        result = await result.json();
+        setName(result.name)
+        setPrice(result.price)
+        setCategory(result.category)
+        setCompany(result.company)
+    };
+
+
+    const updateProduct = async()=>{
+        console.log(name,price,category,company);
+    };
+    
+
+    return (
+        <div className='product'>
+            <h1>Update Product</h1>
+            <input type='text' placeholder='Enter Product name' className='input-box'
+                onChange={(e) => { setName(e.target.value) }} value={name} />
+
+            <input type='text' placeholder='Enter Product price' className='input-box'
+                onChange={(e) => { setPrice(e.target.value) }} value={price} />
+
+            <input type='text' placeholder='Enter Product category' className='input-box'
+                onChange={(e) => { setCategory(e.target.value) }} value={category} />
+
+            <input type='text' placeholder='Enter Product company' className='input-box'
+                onChange={(e) => { setCompany(e.target.value) }} value={company} />
+
+            <button className='appButton'
+            onClick={updateProduct} >Update Product</button>
+        </div>
+    )
+}
+
+export default UpdateProduct;
